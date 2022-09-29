93f44d20b4733c5b555e14e93b0feb6876bd79c5
Aleksei <alxaustralia@gmail.com>
2022-09-29 10:42:03 +0500

    the initial edition

---
+++ Sho-Me Update/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Sho_Me_Update
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Main));
+            this.Button_DownloadBase = new System.Windows.Forms.Button();
+            this.CBox_DriveLetter = new System.Windows.Forms.ComboBox();
+            this.Label_ChoiseCard = new System.Windows.Forms.Label();
+            this.Label_Title = new System.Windows.Forms.Label();
+            this.Picture_Logo = new System.Windows.Forms.PictureBox();
+            this.Label_Status = new System.Windows.Forms.Label();
+            this.CBox_Model = new System.Windows.Forms.ComboBox();
+            this.Button_DownloadFirmware = new System.Windows.Forms.Button();
+            this.CheckBox_Clean = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button_DownloadBase
+            // 
+            this.Button_DownloadBase.Location = new System.Drawing.Point(15, 56);
+            this.Button_DownloadBase.Name = "Button_DownloadBase";
+            this.Button_DownloadBase.Size = new System.Drawing.Size(180, 23);
+            this.Button_DownloadBase.TabIndex = 0;
+            this.Button_DownloadBase.Text = "Загрузить базу";
+            this.Button_DownloadBase.UseVisualStyleBackColor = true;
+            this.Button_DownloadBase.Click += new System.EventHandler(this.Button_DownloadBase_Click);
+            // 
+            // CBox_DriveLetter
+            // 
+            this.CBox_DriveLetter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBox_DriveLetter.FormattingEnabled = true;
+            this.CBox_DriveLetter.Location = new System.Drawing.Point(331, 29);
+            this.CBox_DriveLetter.Name = "CBox_DriveLetter";
+            this.CBox_DriveLetter.Size = new System.Drawing.Size(46, 21);
+            this.CBox_DriveLetter.TabIndex = 2;
+            this.CBox_DriveLetter.SelectedIndexChanged += new System.EventHandler(this.CBox_DriveLetter_SelectedIndexChanged);
+            // 
+            // Label_ChoiseCard
+            // 
+            this.Label_ChoiseCard.AutoSize = true;
+            this.Label_ChoiseCard.Location = new System.Drawing.Point(250, 32);
+            this.Label_ChoiseCard.Name = "Label_ChoiseCard";
+            this.Label_ChoiseCard.Size = new System.Drawing.Size(80, 13);
+            this.Label_ChoiseCard.TabIndex = 3;
+            this.Label_ChoiseCard.Text = "Карта памяти:";
+            // 
+            // Label_Title
+            // 
+            this.Label_Title.AutoSize = true;
+            this.Label_Title.Location = new System.Drawing.Point(14, 9);
+            this.Label_Title.Name = "Label_Title";
+            this.Label_Title.Size = new System.Drawing.Size(208, 13);
+            this.Label_Title.TabIndex = 4;
+            this.Label_Title.Text = "Обновление радар-детекторов SHO-ME";
+            this.Label_Title.Click += new System.EventHandler(this.Label_Title_Click);
+            // 
+            // Picture_Logo
+            // 
+            this.Picture_Logo.Image = global::Sho_Me_Update.Properties.Resources.logo;
+            this.Picture_Logo.Location = new System.Drawing.Point(217, 79);
+            this.Picture_Logo.Name = "Picture_Logo";
+            this.Picture_Logo.Size = new System.Drawing.Size(168, 50);
+            this.Picture_Logo.TabIndex = 5;
+            this.Picture_Logo.TabStop = false;
+            // 
+            // Label_Status
+            // 
+            this.Label_Status.AutoSize = true;
+            this.Label_Status.Location = new System.Drawing.Point(14, 113);
+            this.Label_Status.Name = "Label_Status";
+            this.Label_Status.Size = new System.Drawing.Size(0, 13);
+            this.Label_Status.TabIndex = 6;
+            this.Label_Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Label_Status.Click += new System.EventHandler(this.Label_Status_Click);
+            // 
+            // CBox_Model
+            // 
+            this.CBox_Model.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBox_Model.FormattingEnabled = true;
+            this.CBox_Model.Location = new System.Drawing.Point(16, 29);
+            this.CBox_Model.Name = "CBox_Model";
+            this.CBox_Model.Size = new System.Drawing.Size(216, 21);
+            this.CBox_Model.TabIndex = 7;
+            // 
+            // Button_DownloadFirmware
+            // 
+            this.Button_DownloadFirmware.Location = new System.Drawing.Point(15, 84);
+            this.Button_DownloadFirmware.Name = "Button_DownloadFirmware";
+            this.Button_DownloadFirmware.Size = new System.Drawing.Size(180, 23);
+            this.Button_DownloadFirmware.TabIndex = 8;
+            this.Button_DownloadFirmware.Text = "Загрузить прошивку";
+            this.Button_DownloadFirmware.UseVisualStyleBackColor = true;
+            this.Button_DownloadFirmware.Click += new System.EventHandler(this.Button_Download_Firmware_Click);
+            // 
+            // CheckBox_Clean
+            // 
+            this.CheckBox_Clean.AutoSize = true;
+            this.CheckBox_Clean.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CheckBox_Clean.Checked = true;
+            this.CheckBox_Clean.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckBox_Clean.Location = new System.Drawing.Point(217, 56);
+            this.CheckBox_Clean.Name = "CheckBox_Clean";
+            this.CheckBox_Clean.Size = new System.Drawing.Size(161, 17);
+            this.CheckBox_Clean.TabIndex = 9;
+            this.CheckBox_Clean.Text = "Очистить перед загрузкой";
+            this.CheckBox_Clean.UseVisualStyleBackColor = true;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(393, 138);
+            this.Controls.Add(this.CheckBox_Clean);
+            this.Controls.Add(this.Button_DownloadFirmware);
+            this.Controls.Add(this.CBox_Model);
+            this.Controls.Add(this.Label_Status);
+            this.Controls.Add(this.Label_Title);
+            this.Controls.Add(this.Label_ChoiseCard);
+            this.Controls.Add(this.CBox_DriveLetter);
+            this.Controls.Add(this.Button_DownloadBase);
+            this.Controls.Add(this.Picture_Logo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form_Main";
+            this.Text = "SHO-ME Update";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_DownloadBase;
+        private System.Windows.Forms.ComboBox CBox_DriveLetter;
+        private System.Windows.Forms.Label Label_ChoiseCard;
+        private System.Windows.Forms.Label Label_Title;
+        private System.Windows.Forms.PictureBox Picture_Logo;
+        private System.Windows.Forms.Label Label_Status;
+        private System.Windows.Forms.ComboBox CBox_Model;
+        private System.Windows.Forms.Button Button_DownloadFirmware;
+        private System.Windows.Forms.CheckBox CheckBox_Clean;
+    }
+}
---
+++ Sho-Me Update/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading;
+
+
+
+namespace Sho_Me_Update
+{
+    public partial class Form_Main : Form
+    {
+        List<Model> Models;
+
+        static Model LinkDB;
+        static Model LinkFW;
+        public Form_Main()
+        {
+            InitializeComponent();
+
+            CBox_DriveLetter.DataSource = System.IO.DriveInfo.GetDrives() //Получение списка съемных дисков для выпадающего списка
+            .Where(d => d.DriveType == System.IO.DriveType.Removable).ToList();
+            CBox_DriveLetter.DisplayMember = "Name";
+
+            Models = new List<Model> //Список регистраторов и ссылок для скачивания баз
+            {
+                new Model { Id=1, Name="SHO-ME COMBO SMART SIGNATURE", LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-smart-signature-firmware-rd-sw.zip"},
+                new Model { Id=2, Name="SHO-ME COMBO SLIM SIGNATURE",  LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-slim-signature-firmware-rd-sw.zip"},
+                new Model { Id=3, Name="SHO-ME COMBO №1 SIGNATURE",    LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-signature-firmware-rd-sw.zip"},
+                new Model { Id=4, Name="SHO-ME COMBO DRIVE SIGNATURE", LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-drive-signature-firmware-rd-sw.zip"},
+                new Model { Id=5, Name="SHO-ME COMBO MINI WiFi",       LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-mini-wifi-signature-firmware-rd-sw.zip"},
+                new Model { Id=6, Name="",                             LinkDB="",                                                                             LinkFW=""},
+                new Model { Id=7, Name="SHO-ME COMBO №1",              LinkDB="https://sho-me.ru/downloads/database/archive/sho-me-combo-1-db.zip",           LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/sho-me-combo-1-dec.zip"},
+                new Model { Id=8, Name="SHO-ME COMBO №1 A7",           LinkDB="https://sho-me.ru/downloads/database/combo_database.zip",                      LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/27-12-2018/sho-me-combo-1-a7.zip"},
+                new Model { Id=9, Name="SHO-ME COMBO №3",              LinkDB="https://sho-me.ru/downloads/database/archive/sho-me-combo-n3-db.zip",          LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/17-03-2016/sho-me-combo-3-software-at-30-november.zip"},
+                new Model { Id=10, Name="SHO-ME COMBO №3 A7",          LinkDB="https://sho-me.ru/downloads/database/sho-me-combo-n3a7-n5a7-wombat-db.zip",    LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/combo/28-08-2018/sho-me-combo-3-a7-firmware-sn-1511.zip"},
+                new Model { Id=11, Name="SHO-ME COMBO №5 A7",          LinkDB="https://sho-me.ru/downloads/database/sho-me-combo-n3a7-n5a7-wombat-db.zip",    LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/combo/28-08-2018/sho-me-combo-5-a7-firmware.zip"},
+                new Model { Id=12, Name="SHO-ME COMBO №5 A12",         LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-n5-a12-firmware-rd-sw.zip"},
+                new Model { Id=13, Name="SHO-ME COMBO SLIM",           LinkDB="https://sho-me.ru/downloads/database/combo_database.zip",                      LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/27-12-2018/sho-me-combo-slim.zip"},
+                new Model { Id=14, Name="SHO-ME COMBO SMART",          LinkDB="https://sho-me.ru/downloads/database/combo_database.zip",                      LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/27-12-2018/sho-me-combo-smart.zip"},
+                new Model { Id=15, Name="SHO-ME COMBO WOMBAT",         LinkDB="https://sho-me.ru/downloads/database/sho-me-combo-n3a7-n5a7-wombat-db.zip",    LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/combo/28-08-2018/sho-me-combo-wombat-firmware.zip"},
+                new Model { Id=16, Name="SHO-ME COMBO №3 iCatch",      LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-3-i-catch-firmware-rd-sw.zip"},
+                new Model { Id=17, Name="SHO-ME COMBO SUPER SMART",    LinkDB="https://speedcam.online/rd.online/shome/shome3/combo_database.zip",            LinkFW="https://sho-me.ru/downloads/obnovleniya/firmware/14-08-2020/sho-me-combo-super-smart-firmware-rd-sw.zip"},
+            };
+
+            CBox_Model.DataSource = Models; //Источник данных для выпадающего списка выбора модели регистратора
+            CBox_Model.ValueMember = "Id";
+            CBox_Model.DisplayMember = "Name";
+            CBox_Model.SelectedIndexChanged += CBox_Model_SelectedIndexChanged;
+            CheckBox_Clean.CheckedChanged += CheckBox_Clean_CheckedChanged;
+        }
+        class Model
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string LinkDB { get; set; }
+            public string LinkFW { get; set; }
+        }
+        public static bool CheckForInternetConnection(out string msg) //Проверка доступа в интернет
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead(new Uri("https://google.com")))
+                {
+                    msg = "";
+                    return true;
+                }
+            }
+            catch
+            {
+                msg = "Сеть недоступна";
+                return false;
+            }
+        }
+        public static bool CheckDownloadLinkDB(out string msg) //Проверка доступа ссылки
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead(new Uri(LinkDB.LinkDB)))
+                {
+                    msg = "";
+                    return true;
+                }
+            }
+            catch
+            {
+                msg = "Не найдена база на сайте";
+                return false;
+            }
+        }
+        public static bool CheckDownloadLinkFW(out string msg) //Проверка доступа ссылки
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead(new Uri(LinkFW.LinkFW)))
+                {
+                    msg = "";
+                    return true;
+                }
+            }
+            catch
+            {
+                msg = "Не найдена база на сайте";
+                return false;
+            }
+        }
+        public static bool CheckDriveLetter(string path, out string msg) //Проверка карты памяти
+        {
+            try
+            {
+                var dir = new DirectoryInfo(path);
+                if (dir.Exists)
+                {
+                    msg = string.Empty;
+                    return true;//The directory exists
+                }
+                else
+                {
+                    msg = "Карта памяти недоступна";
+                    return false;//The path is valid, but does not exist.
+                }
+            }
+            catch
+            {
+                msg = "Карта памяти недоступна";
+                return false;//The path is invalid or user does not have access.
+            }
+        }
+        void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        void Label_Title_Click(object sender, EventArgs e) //Заголовок
+        {
+        }
+        void CBox_Model_SelectedIndexChanged(object sender, EventArgs e) //Выпадающий список выбора моделей регистраторов
+        {
+            Label_Status.Text = null;
+            LinkDB = (Model)CBox_Model.SelectedItem;
+        }
+        void CBox_DriveLetter_SelectedIndexChanged(object sender, EventArgs e) //Выпадающий список выбора съемного диска
+        {
+            Label_Status.Text = null;
+        }
+        void CheckBox_Clean_CheckedChanged(object sender, EventArgs e)
+        {
+            //CheckBox CheckBox_Clean = (CheckBox)sender;
+        }
+        void Button_DownloadBase_Click(object sender, EventArgs e) //Кнопка загрузки базы данных на съемный диск
+        {
+            Label_Status.Text = "Загрузка...";
+
+            LinkDB = (Model)CBox_Model.SelectedItem;
+            var path = CBox_DriveLetter.SelectedValue != null ? CBox_DriveLetter.SelectedItem.ToString() : "";
+            if (CheckForInternetConnection(out string errorMsg)
+                    && CheckDownloadLinkDB(out errorMsg)
+                    && CheckDriveLetter(path, out errorMsg))
+            {
+                var Drive = CBox_DriveLetter.SelectedItem.ToString();
+                var FileDB = Drive + "ComboDB.bin"; //Переменная для файла
+                var ArchiveDB = Drive + "combo_database.zip"; //Переменная для архива
+
+                System.IO.DirectoryInfo drive = new DirectoryInfo(Drive);
+                if (CheckBox_Clean.Checked == true)
+                    foreach (FileInfo file in drive.EnumerateFiles())
+                    {
+                    file.Delete();
+                    }
+                Thread.Sleep(1000);
+
+                var downloadDB = new WebClient(); //Скачивание архива
+                downloadDB.DownloadFile(LinkDB.LinkDB, ArchiveDB);
+
+                if (File.Exists(ArchiveDB)) //Проверка существования загруженного архива
+                {
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(ArchiveDB, Drive); //Распаковка архива
+                        File.Delete(ArchiveDB);
+                        Label_Status.Text = "Готово!";
+                    }
+                    catch
+                    {
+                        Label_Status.Text = "Ошибка распаковки архива. Очистите карту памяти";
+                    }
+                }
+                else
+                {
+                    Label_Status.Text = "Загрузка базы не выполнена";
+                }
+            }
+            else
+            {
+                Label_Status.Text = errorMsg;
+            }
+        }
+        void Button_Download_Firmware_Click(object sender, EventArgs e) //Кнопка загрузки прошивки на съемный диск
+        {
+            Label_Status.Text = "Загрузка...";
+            var Drive = CBox_DriveLetter.SelectedItem.ToString();
+            var FileFW = Drive + "firmware.bin"; //Переменная для файла
+            var ArchiveFW = Drive + "firmware.zip"; //Переменная для архива
+
+            LinkFW = (Model)CBox_Model.SelectedItem;
+            var path = CBox_DriveLetter.SelectedValue != null ? Drive : "";
+            if (CheckForInternetConnection(out string errorMsg)
+                    && CheckDownloadLinkFW(out errorMsg)
+                    && CheckDriveLetter(path, out errorMsg))
+            {
+                System.IO.DirectoryInfo drive = new DirectoryInfo(Drive);
+                if (CheckBox_Clean.Checked == true)
+                    foreach (FileInfo file in drive.EnumerateFiles())
+                    {
+                        file.Delete();
+                    }
+                Thread.Sleep(1000);
+
+                var downloadFW = new WebClient(); //Скачивание архива
+                downloadFW.DownloadFile(LinkFW.LinkFW, ArchiveFW);
+
+                if (File.Exists(ArchiveFW)) //Проверка существования загруженного архива
+                {
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(ArchiveFW, Drive); //Распаковка архива
+                        File.Delete(ArchiveFW);
+                        Label_Status.Text = "Готово!";
+                    }
+                    catch
+                    {
+                        Label_Status.Text = "Ошибка распаковки архива. Очистите карту памяти";
+                    }
+                }
+                else
+                {
+                    Label_Status.Text = "Загрузка базы не выполнена";
+                }
+            }
+            else
+            {
+                Label_Status.Text = errorMsg;
+            }
+        }
+
+        void Label_Status_Click(object sender, EventArgs e) //Текст статуса и вывод ошибок
+        {
+        }
+        void Button_Exit_Click(object sender, EventArgs e) //Кнопка выхода из программы
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
